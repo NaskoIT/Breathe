e0632db978f20a8ec09c49d0ed4907c7fb23b729
Aneta <aneta@limechain.tech>
2021-02-26 22:26:00 +0200

    Added store structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import classes from "./App.module.scss";
+
+function App() {
+  return <div className={classes.App}></div>;
+}
+
+export default App;
---
+++ frontend/src/store/models/index.js
@@ -0,0 +1,3 @@
+import { user } from './user.model';
+
+export default { user };
